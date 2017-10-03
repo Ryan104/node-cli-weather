b1a3e3b1a3db207c0f1e304e9196be8df9727663
Ryan <ryan.d.elliott@gmail.com>
2017-10-03 14:21:05 -0600

    Get weather for given location

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-//Bonus: take in state and city as arguments instead of hardcoding
+const request = require('request');
+const prompt = require('prompt');
+const { wundergroundKey } = require('./env');
 
 
-//Require apiKey
+let state = "CO";
+let city = "Denver";
 
-//Require the request library
-//What is the `request` library you ask?
-//Google `npm request` and you can find out!
+function makeURL(state, city, key){
+	return `http://api.wunderground.com/api/${key}/conditions/q/${state}/${city}.json`;
+}
 
+prompt.start();
+prompt.get(['state', 'city'], function(err, result){
+	if (err) throw err;
 
-// Set the API URL we are hitting, with our API Key and zipcode
+	let url = makeURL(result.state, result.city, wundergroundKey);
 
-//The actual request sending
-
-request(apiUrl, function (error, response, body) {
-  //Inside that callback
-
-  //Print out the weather result
+	request(url, function (error, response, body) {
+	  try {
+	  	let data = JSON.parse(body).current_observation;
+	  	console.log('The weather in ' + data.display_location.full +' is ' + data.weather);
+	  	console.log('The temp is ' + data.temp_f);
+	  } catch(e) {
+	  	console.log('error: ' + e);
+	  }
+	  
+	});
 });
+
